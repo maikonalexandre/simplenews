ab4b5b09be5238ff7faf801e61260598a795f32f
Maike <maikonalexandre574@gmail.com>
2024-06-06 19:15:43 -0300

    feat: create footer component

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,11 @@
+export const Footer = () => {
+  return (
+    <footer className="bg-zinc-100 w-full p-2">
+      <div className="max-w-[1140px] mx-auto flex justify-center">
+        <span className="text-sm font-medium text-zinc-700">
+          Online diary © 2024
+        </span>
+      </div>
+    </footer>
+  )
+}
