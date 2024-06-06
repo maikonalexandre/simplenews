572278af19fa46b5ec1e146a25baccde9011db36
Maike <maikonalexandre574@gmail.com>
2024-06-06 19:14:41 -0300

    feat: create fonts  config

---
+++ src/config/fonts.ts
@@ -0,0 +1,5 @@
+import { Bebas_Neue, Inter, Pacifico } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+export const bebasNeue = Bebas_Neue({ weight: '400', subsets: ['latin'] })
+export const pacifico = Pacifico({ weight: '400', subsets: ['latin'] })
